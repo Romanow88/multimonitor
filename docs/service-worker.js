5fd2b1e7ec9ec0c2a0f915b764c6654f0c7805bf
rdoboni <romanow88@gmail.com>
2021-10-05 14:26:39 +0200

    adapt service worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,23 +1,9 @@
-const cacheName = "app-cache";
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    await cache.put(e.request, response.clone());
-    return response;
-  })());
-});
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key === cacheName) { return; }
-      return caches.delete(key);
-    }))
-  }));
+workbox.clientsClaim();
+
+workbox.routing.registerNavigationRoute("/multimonitor/index.html", {
+
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
